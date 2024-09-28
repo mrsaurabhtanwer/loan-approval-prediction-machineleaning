d6da88ce78eb5bedbf4ad526f2ba7cd9b62fa803
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2024-09-28 18:26:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,6 @@ import matplotlip as plt.py
 st.title(' Loan Approval Prediction')
 
 st.write(' Here you can predict your loan eligibility certria')
+ df=pd.read_csv("https://github.com/mrsaurabhtanwer/Loan-Approval-Prediction-using-Machine-Learning-/blob/main/Copy%20of%20loan%20-%20loan.csv")
+
 
