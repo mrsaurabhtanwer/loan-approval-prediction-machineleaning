615ea48f6cf5dcb2ce90eba9dc02e959f8f16f24
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2024-09-28 18:31:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ from sklearn import svm
 st.title(' Loan Approval Prediction')
 
 st.write(' Here you can predict your loan eligibility certria')
-df=pd.read_csv("https://github.com/mrsaurabhtanwer/Loan-Approval-Prediction-using-Machine-Learning-/blob/main/Copy%20of%20loan%20-%20loan.csv")
+df=pd.read_csv('https://github.com/mrsaurabhtanwer/Loan-Approval-Prediction-using-Machine-Learning-/blob/main/Copy%20of%20loan%20-%20loan.csv')
 df
 
