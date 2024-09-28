1c9fc2027c680a7fa508b2d20a7bd6bc99b291b3
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2024-09-28 18:33:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
+
 import seaborn as sns
 from sklearn import svm
 
 
 st.title(' Loan Approval Prediction')
 
-st.write(' Here you can predict your loan eligibility certria')
+st.info(' Here you can predict your loan eligibility certria')
 df=pd.read_csv('https://github.com/mrsaurabhtanwer/Loan-Approval-Prediction-using-Machine-Learning-/blob/main/Copy%20of%20loan%20-%20loan.csv')
 df
 
