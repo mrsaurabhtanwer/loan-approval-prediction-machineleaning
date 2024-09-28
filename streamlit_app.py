97caa0b010c0b1bc202f220d12651d8344b097ec
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2024-09-28 18:22:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd 
+import numpy as np
+import sktlearn as skt
+import matplotlip as plt.py
+
 
 st.title(' Loan Approval Prediction')
 
 st.write(' Here you can predict your loan eligibility certria')
+
