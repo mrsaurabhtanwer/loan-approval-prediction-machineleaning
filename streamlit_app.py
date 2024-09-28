62e0a731a18e073d81d97a9a0f5278dee6dbf9d6
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2024-09-28 19:57:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as 
 File"/mount/src/loan-approval-prediction-machineleaning/streamlit_app.py"
-def fetch_data(https://github.com/mrsaurabhtanwer/Loan-Approval-Prediction-using-Machine-Learning-/blob/main/Copy%20of%20loan%20-%20loan.csv)
+
 
 st.title(' Loan Approval Prediction')
 
