4106c033d25f104a503284a72b52c29c807dd793
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2024-09-28 19:57:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as 
-File"/mount/src/loan-approval-prediction-machineleaning/streamlit_app.py"
-
+import numpy as np
 
 st.title(' Loan Approval Prediction')
 
