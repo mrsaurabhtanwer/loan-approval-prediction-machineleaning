e30cd082d90b2c703c745640d0d6331b6efdad83
Saurabh Tanwer <86065599+mrsaurabhtanwer@users.noreply.github.com>
2024-09-28 18:50:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ import numpy as np
 st.title(' Loan Approval Prediction')
 
 st.info(' Here you can predict your loan eligibility certria')
-df =pd.read_csv('https://github.com/mrsaurabhtanwer/loan-approval-prediction-machineleaning/blob/master/Copy%20of%20loan%20-%20loan.csv')
+df =pd.read_csv('/master/Copy%20of%20loan%20-%20loan.csv')
 df
 
